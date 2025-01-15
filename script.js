76e6d305837352cfea79dd2340b8a89ffa796909
fahad09011 <fahadimran038@gmail.com>
2025-01-15 21:33:40 +0000

    Your branch is up to date with 'origin/main'. 	modified:   index.html 	modified:   script.js 	modified:   style.css

--- script.js
+++ script.js
@@ -1,27 +1,35 @@
-// buttons
-let aboutButton = document.getElementById("about-button")
-let skillsButton = document.getElementById("skills-button")
-let reviewButton = document.getElementById("review-button")
-// button content
-let aboutContent = document.getElementById("about-content")
-let skillsContent = document.getElementById("skills-content")
-let reviewContent = document.getElementById("review-content")
+document.addEventListener("DOMContentLoaded",()=>{
 
-aboutButton.addEventListener("click", ()=>{
-    functionOnClick(aboutContent);
-});
 
-skillsButton.addEventListener("click", ()=>{
-    functionOnClick(skillsContent);
-});
-reviewButton.addEventListener("click", ()=>{
-    functionOnClick(reviewContent);
-});
-
- function functionOnClick(activebutton){
-    let allcontent = document.querySelectorAll(".button-content");
-    allcontent.forEach((content)=>{
-        content.classList.remove("active")
+    let aboutButton = document.getElementById("about-button")
+    let skillsButton = document.getElementById("skills-button")
+    let reviewButton = document.getElementById("review-button")
+    // button content
+    let aboutContent = document.getElementById("about-content")
+    let skillsContent = document.getElementById("skills-content")
+    let reviewContent = document.getElementById("review-content")
+    
+    
+    // this functionOnClick take parameter (clicked one  button)
+     function functionOnClick(activebutton){
+        let allcontent = document.querySelectorAll(".button-content");
+        allcontent.forEach((content)=>{
+            content.classList.remove("active")
+        });
+    activebutton.classList.add("active");
+    }
+    
+    functionOnClick(reviewContent); // this function shows the about me content always until click on button
+    
+    aboutButton.addEventListener("click", ()=>{
+        functionOnClick(aboutContent);
     });
-activebutton.classList.add("active");
-}
+    
+    skillsButton.addEventListener("click", ()=>{
+        functionOnClick(skillsContent);
+    });
+    reviewButton.addEventListener("click", ()=>{
+        functionOnClick(reviewContent);
+    });
+});
+// buttons
