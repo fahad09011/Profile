f9313652aaeb531096ab58f9951164e7a429d075
fahad09011 <fahadimran038@gmail.com>
2025-01-16 10:44:31 +0000

    Your branch is up to date with 'origin/main'. 	modified:   index.html modified:   script.js 	modified:   style.css

--- script.js
+++ script.js
@@ -32,4 +32,50 @@ document.addEventListener("DOMContentLoaded",()=>{
         functionOnClick(reviewContent);
     });
 });
-// buttons
+//<================ form JS ===============>
+    // main page appointment button
+    let appointmentButton = document.getElementById("booking-button")
+    let closebutton = document.getElementById("close")
+    // appointment button in form
+    let confirmAppointmentButton = document.getElementById("booking-button-in-form")
+    // form section
+    let formsection = document.getElementById("form-section")
+    // form 
+    let form = document.getElementById("form")
+//  to get form data
+let clientName =document.getElementById("name")
+let address =document.getElementById("address")
+let email =document.getElementById("email")
+let phone =document.getElementById("phone")
+let date =document.getElementById("date")
+let time =document.getElementById("time")
+
+
+
+//  to display form
+    appointmentButton.addEventListener("click",()=>{
+formsection.style.display="flex"
+    });
+    // to close form by clicking on crose button
+    closebutton.addEventListener("click",()=>{
+        formsection.style.display="none"
+    });
+
+    // handle form submission
+    form.addEventListener("submit",(e)=>{
+        e.preventDefault();
+
+    alert(`Appointment Booked Successfully !
+        With Details:
+         Name:  ${clientName.value}
+         Email: ${email.value}
+         Phone: ${phone.value}
+         Date:  ${date.value}
+         Time:  ${time.value}`)
+
+         formsection.style.display="none";
+         form.reset();
+    });
+
+
+
